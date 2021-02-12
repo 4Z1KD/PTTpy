a236976e7ff48b8db529fa8c8bcfbb4c6e595ec3
Gil (4Z1KD) <4z1kd@iarc.org>
2021-02-12 18:04:33 +0200

    js8Call version

---
+++ js8Call/js8_ptt_listener.py
@@ -0,0 +1,52 @@
+import time
+import asyncio
+import serial
+import serial_asyncio
+import sys
+
+def log(msg):
+    t = time.localtime()
+    current_time = time.strftime("%D %H:%M:%S", t)
+    print(f"{current_time}  {msg}")
+
+class Output(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+        #self.transport.serial.rts = True #set RTS to HIGH
+        log(f"port opened")
+        log(f"{transport}")
+
+    def data_received(self, data):
+        print("data received", repr(data))
+        if data == b"on":
+            ser.setRTS(True)
+            log("sending...")
+        elif data == b"off":
+            ser.setRTS(False)
+            log("receiving...")
+        if b"\n" in data:
+            log("closing...")
+            self.transport.close()
+
+    def connection_lost(self, exc):
+        log("port closed")
+        self.transport.loop.stop()
+
+    def pause_writing(self):
+        log("pause writing")
+        log(self.transport.get_write_buffer_size())
+
+    def resume_writing(self):
+        log(self.transport.get_write_buffer_size())
+        log("resume writing")
+if __name__ == "__main__":
+    print(sys.version)
+    global ser
+    ser = serial.Serial("COM9")
+    ser.setRTS(False)
+
+    loop = asyncio.get_event_loop()
+    coro = serial_asyncio.create_serial_connection(loop, Output, "COM12", baudrate=38400)
+    loop.run_until_complete(coro)
+    loop.run_forever()
+    loop.close()
