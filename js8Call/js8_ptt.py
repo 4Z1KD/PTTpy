6950260772911651e6598b629462b85addd36ddd
Gil (4Z1KD) <4z1kd@iarc.org>
2021-03-06 11:33:12 +0200

    Delete js8Call directory

--- js8Call/js8_ptt.py
+++
@@ -1,7 +0,0 @@
-import serial
-import sys
-
-#this application expects "on" or "off" as argv and sends it through the serial port
-ser = serial.Serial("COM11") #set the virtual com port number
-b = sys.argv[1].encode('utf-8') #convert to bytes
-ser.write(b) #send
